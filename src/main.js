138c0612988ab7ddfddbff5635ab0a119b47d882
Bailey Russo <BRusso@cccsd.org>
2025-03-31 13:32:00 -0400

    init

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from "vue"
+import vuetify from "./plugins/vuetify"
+import { loadFonts } from "./plugins/webfontloader"
+import App from "./App.vue"
+
+loadFonts()
+
+createApp(App).use(vuetify).mount("#app")
